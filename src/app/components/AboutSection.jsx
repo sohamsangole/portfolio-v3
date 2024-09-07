32d83991063b3b689c2ec5ed91bb22ba02c02993
Soham Sangole <sohamajaysangole@gmail.com>
2024-09-07 23:15:36 +0530

    Updated the Background

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -16,8 +16,8 @@ const AboutSection = () => {
     <section className="text-white h-screen flex items-center justify-end " id="about">
       <div className="flex flex-col md:flex-row items-center md:justify-end gap-8 md:gap-16">
         <div className="text-left max-w-xl">
-          <h2 className="text-4xl font-bold mb-4">About Me</h2>
-          <p className="text-[#ADB7BE] text-base sm:text-lg lg:text-2xl">
+          <h2 className="text-white text-4xl font-bold mb-4">About Me</h2>
+          <p className="text-white text-base sm:text-lg lg:text-2xl">
             A passionate and driven technology enthusiast, I focus on building innovative solutions through deep learning, app development, and web technologies. With hands-on experience in machine learning and app development, I am constantly exploring new ways to apply my skills in impactful projects. Having worked on applications ranging from healthcare to smart music recommendations, my goal is to leverage cutting-edge technology to solve real-world problems. Whether leading teams or collaborating on complex projects, I am committed to pushing the boundaries of what&apos;s possible in the tech space.
           </p>
         </div>
--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -3,13 +3,13 @@ import ContactLinks from "./ContactLinks";
 
 const Footer = () => {
   return (
-    <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
-      <div className="container p-12 flex justify-between">
-        <span>Soham Sangole</span>
+    <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white flex flex-col items-center">
+      <div className="container p-12 flex flex-col items-center justify-center space-y-4 md:flex-row md:space-y-0 md:justify-between">
+        <span className="text-center">Soham Sangole</span>
         <div className="flex space-x-3 md:space-x-8">
           <ContactLinks />
         </div>
-        <p className="text-white hidden md:block">2024</p>
+        <p className="text-white">2024</p>
       </div>
     </footer>
   );
