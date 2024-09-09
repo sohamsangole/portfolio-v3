0512afba52275cb912e2d29f73ab6cd2407b4c7a
Soham Sangole <sohamajaysangole@gmail.com>
2024-09-10 01:21:57 +0530

    Corrected default light mode

--- src/app/context/ThemeContext.js
+++ src/app/context/ThemeContext.js
@@ -7,12 +7,20 @@ import { lightTheme, darkTheme } from "../theme";
 export const ThemeContext = createContext();
 
 export const ThemeProvider = ({ children }) => {
-    const [isDarkMode, setIsDarkMode] = useState(true);
+    const [isDarkMode, setIsDarkMode] = useState(false);
     const [theme, setTheme] = useState(lightTheme);
 
     useEffect(() => {
         const savedTheme = localStorage.getItem("theme");
-        if (savedTheme === "light") {
+        if (savedTheme) {
+            if (savedTheme === "dark") {
+                setIsDarkMode(true);
+                setTheme(darkTheme);
+            } else {
+                setIsDarkMode(false);
+                setTheme(lightTheme);
+            }
+        } else {
             setIsDarkMode(false);
             setTheme(lightTheme);
         }
