f80ddf6d418d838d714f390ae8df7755fba6c4fb
Soham Sangole <sohamajaysangole@gmail.com>
2024-11-08 13:48:54 +0530

    Updated education and experience section

--- src/app/components/eduexp.js
+++ src/app/components/eduexp.js
@@ -4,22 +4,23 @@ import Education from './education';
 import Experience from './experience';
 
 const EducationExperience = () => {
-    const [showEducation, setShowEducation] = useState(true);
+    const [showEducation, setShowEducation] = useState(false);
 
     return (
         <div>
             <div className="flex mb-4">
-                <button
-                    onClick={() => setShowEducation(true)}
-                    className={`flex-1 py-2 rounded ${showEducation ? "bg-blue-500 text-white" : "bg-gray-100 text-[#777777]"}`}
-                >
-                    <strong>Education</strong>
-                </button>
+
                 <button
                     onClick={() => setShowEducation(false)}
                     className={`flex-1 py-2 rounded ${!showEducation ? "bg-blue-500 text-white" : "bg-gray-100 text-[#777777]"}`}
                 >
                     <strong>Experience</strong>
+                </button>
+                <button
+                    onClick={() => setShowEducation(true)}
+                    className={`flex-1 py-2 rounded ${showEducation ? "bg-blue-500 text-white" : "bg-gray-100 text-[#777777]"}`}
+                >
+                    <strong>Education</strong>
                 </button>
             </div>
 
