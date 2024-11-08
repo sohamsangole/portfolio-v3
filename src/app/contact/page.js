4147836821d58ba420ecfaecbe4226ecabff8f6c
Soham Sangole <sohamajaysangole@gmail.com>
2024-11-08 13:41:39 +0530

    Added Contact Me

---
+++ src/app/contact/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { NavBar } from '../components/navbar/navbar'
+
+const page = () => {
+    return (
+        <div className="w-[50%] mx-auto py-4">
+            <NavBar />
+        </div>
+    )
+}
+
+export default page
