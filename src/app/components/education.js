7ff1274dd78ee32395e210cc3dc5628401727df0
Soham Sangole <sohamajaysangole@gmail.com>
2024-11-19 17:44:02 +0530

    updated edu and exp for smallers screens

--- src/app/components/education.js
+++ src/app/components/education.js
@@ -4,11 +4,10 @@ import Image from 'next/image';
 const Education = () => {
     return (
         <div className="relative shadow-[4px_4px_8px_rgba(0,0,0,0.15)] rounded-[10px] overflow-hidden">
-            {/* <div className="text-black font-bold text-[18px] mb-4">Education</div> */}
-            <div className="absolute left-[55px] top-[0px] h-[100%] w-[1px] opacity-50 bg-black -z-10"></div>
-            <div className='p-[20px]'>
-                <div className="flex items-center mb-10 relative">
-                    <div className="bg-white">
+            <div className="absolute left-[55px] top-[0px] h-[100%] w-[1px] opacity-50 bg-black -z-10 hidden lg:block"></div>
+            <div className='p-[20px] flex flex-col items-center'>
+                <div className="flex flex-col lg:flex-row items-center lg:items-start mb-10 relative w-full">
+                    <div className="bg-white mb-4 lg:mb-0">
                         <Image
                             src="/vjti.png"
                             alt="VJTI Logo"
@@ -17,36 +16,41 @@ const Education = () => {
                             className="mr-4"
                         />
                     </div>
-                    <div>
+                    <div className="text-center lg:text-left">
                         <div className="text-[#777777]">2021 - 2025</div>
                         <div className='font-bold'>Veermata Jijabai Technological Institute</div>
                         <div className='font-bold text-[#777777]'>B.Tech. Information Technology</div>
                     </div>
                 </div>
-                <div className="flex items-center mb-10 relative">
-                    <Image
-                        src="/school.jpg"
-                        alt="VJTI Logo"
-                        height={70}
-                        width={70}
-                        className="mr-4 rounded-full"
-                    />
-                    <div>
+
+                <div className="flex flex-col lg:flex-row items-center lg:items-start mb-10 relative w-full">
+                    <div className="mb-4 lg:mb-0">
+                        <Image
+                            src="/school.jpg"
+                            alt="Golden Kids College"
+                            height={70}
+                            width={70}
+                            className="rounded-full mr-4"
+                        />
+                    </div>
+                    <div className="text-center lg:text-left">
                         <div className="text-[#777777]">2019 - 2021</div>
                         <div className='font-bold'>Golden Kids English Jr. College</div>
                         <div className='font-bold text-[#777777]'>Higher Secondary Certificate</div>
                     </div>
                 </div>
-                <div className="flex items-center relative">
-                    {/* <div className="w-[70px] h-[70px] bg-[#F3F3F3] rounded-full mr-4"></div> */}
-                    <Image
-                        src="/school.jpg"
-                        alt="VJTI Logo"
-                        height={70}
-                        width={70}
-                        className="mr-4 rounded-full"
-                    />
-                    <div>
+
+                <div className="flex flex-col lg:flex-row items-center lg:items-start relative w-full">
+                    <div className="mb-4 lg:mb-0">
+                        <Image
+                            src="/school.jpg"
+                            alt="Golden Kids High School"
+                            height={70}
+                            width={70}
+                            className="mr-4 rounded-full"
+                        />
+                    </div>
+                    <div className="text-center lg:text-left">
                         <div className="text-[#777777]">2009 - 2019</div>
                         <div className='font-bold'>Golden Kids English High School</div>
                         <div className='font-bold text-[#777777]'>Secondary School Certificate</div>
--- src/app/components/experience.js
+++ src/app/components/experience.js
@@ -4,27 +4,10 @@ import Image from 'next/image';
 const Experience = () => {
     return (
         <div className="relative shadow-[4px_4px_8px_rgba(0,0,0,0.15)] rounded-[10px] overflow-hidden">
-            {/* <div className="text-black font-bold text-[18px] mb-4">Experience</div> */}
-            <div className="absolute left-[55px] top-[0px] h-[100%] w-[1px] opacity-50 bg-black -z-10"></div>
-            <div className='p-[20px]'>
-                {/* <div className="flex items-center mb-8 relative">
-                    <div className="bg-white">
-                        <Image
-                            src="/bofa.png"
-                            alt="Company Logo"
-                            height={70}
-                            width={70}
-                            className="mr-4"
-                        />
-                    </div>
-                    <div>
-                        <div className="text-[#777777]">August 2025</div>
-                        <div className='font-bold'>Bank Of America</div>
-                        <div className='font-bold text-[#777777]'>SDE</div>
-                    </div>
-                </div> */}
-                <div className="flex items-center mb-8 relative">
-                    <div className="bg-white px-[10px]">
+            <div className="absolute left-[55px] top-[0px] h-[100%] w-[1px] opacity-50 bg-black -z-10 hidden lg:block"></div>
+            <div className='p-[20px] flex flex-col items-center'>
+                <div className="flex flex-col lg:flex-row items-center lg:items-start mb-8 relative w-full">
+                    <div className="bg-white px-[10px] mb-4 lg:mb-0">
                         <Image
                             src="/logo-red.png"
                             alt="Company Logo"
@@ -33,15 +16,16 @@ const Experience = () => {
                             className="mr-4"
                         />
                     </div>
-                    <div>
+                    <div className="text-center lg:text-left">
                         <div className="text-[#777777]">June 2024 - July 2024</div>
                         <div className='font-bold'>Thermax Limited</div>
                         <div className='font-bold text-[#777777]'>Summer Intern</div>
                     </div>
                 </div>
-                <div className="flex items-center relative">
-                    <div className="w-[70px] h-[70px] bg-[#F3F3F3] rounded-full mr-4"></div>
-                    <div>
+
+                <div className="flex flex-col lg:flex-row items-center lg:items-start relative w-full">
+                    <div className="w-[70px] h-[70px] bg-[#F3F3F3] rounded-full mr-4 mb-4 lg:mb-0"></div>
+                    <div className="text-center lg:text-left">
                         <div className="text-[#777777]">June 2023 - Present</div>
                         <div className='font-bold'>KEM Hospital</div>
                         <div className='font-bold text-[#777777]'>App Developer</div>
