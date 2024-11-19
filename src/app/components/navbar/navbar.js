a93df1c45036d53b46d62e3434ef0f42af2ba44c
Soham Sangole <sohamajaysangole@gmail.com>
2024-11-19 17:31:52 +0530

    Updated navbar

--- src/app/components/navbar/navbar.js
+++ src/app/components/navbar/navbar.js
@@ -6,7 +6,7 @@ const NavBarComponent = forwardRef((props, ref) => {
 
     return (
         <nav className={styles.nav}>
-            <div className="container py-4 flex justify-start items-center">
+            <div className="container py-4 flex justify-start items-center sm:px-0 px-3">
                 <ul className="flex space-x-10 text-black font-bold text-[18px]">
                     <li>
                         <Link href="/">Hello !</Link>
