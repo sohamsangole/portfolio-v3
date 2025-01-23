11352951f3fab6203033f06fe398e892c4af4d9e
Soham Sangole <sohamajaysangole@gmail.com>
2025-01-24 04:41:02 +0530

    year updated

--- src/app/components/footer.js
+++ src/app/components/footer.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div className="bg-white text-black py-6">
+        <div className="text-black py-6">
             <div className="container mx-auto text-center">
-                <p className="text-sm">© 2024 Soham Sangole.</p>
+                <p className="text-sm">© 2025 Soham Sangole.</p>
                 <p className="text-sm mt-2">Made with ❤️</p>
             </div>
         </div>
